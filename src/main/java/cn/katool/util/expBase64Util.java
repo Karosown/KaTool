2c664c361764f07cdd0d34b781c21979f6226b93
Karos <mail@wzl1.top>
2023-02-09 02:02:49 +0800

    2023-2-9 new expBase64Util.java

--- src/main/java/cn/katool/util/expBase64Util.java
+++ src/main/java/cn/katool/util/expBase64Util.java
@@ -13,14 +13,21 @@ package cn.katool.util;
 import org.springframework.util.Base64Utils;
 
 public class expBase64Util extends Base64Utils {
+    //Base64 code regular judgment expression
     private static final String BASE64_PATTERN = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$";
-    public static Boolean isBase64(String base64){
-        if (base64==null||base64.length()==0){
+
+    /**
+     * To determine whether a string is a base64 expression, you can use the Base64 header
+     * @param base64str
+     * @return Returns a result indicating whether the current base64str meets the base64 code
+     */
+    public static boolean isBase64(String base64str){
+        if (base64str==null||base64str.length()==0){
             return false;
         }
-        if (base64.length()>=11&"data:image/".equals(base64.substring(0,11))){
-            base64=base64.substring(base64.indexOf(',')+1);
+        if (base64str.length()>=11&"data:image/".equals(base64str.substring(0,11))){
+            base64str=base64str.substring(base64str.indexOf(',')+1);
         }
-        return base64.matches(BASE64_PATTERN);
+        return base64str.matches(BASE64_PATTERN);
     }
 }
